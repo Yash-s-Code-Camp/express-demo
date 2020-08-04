c33c6e4e6bed4751bbc0b6fc9420a28dba8288f8
yddc902 <yddc902@live.com>
2020-08-04 07:54:05 +0530

    added nodemon and script

--- routes/teacher_routes.js
+++ routes/teacher_routes.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get('/all', (req, res) => {
     // todo fetch all teachers from db
-    res.send(`working`)
+    res.send(`working........`)
 })
 router.get('/id/:id', (req, res) => {
     // todo fetch a single teacher with particular id from db
